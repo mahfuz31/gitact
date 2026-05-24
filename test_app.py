c019f59ed29f5eb2ef5203d301fa286f6aea8721
mahfuz31 <mahfuz.knu@gmail.com>
2026-05-24 18:25:27 +0600

    feat: added pytest unit testing suite to pipeline

--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
-from app import add
+from app import add_numbers
 
-def test_add():
-    assert add(2,3) == 5
+def test_add_numbers_success():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+
+def test_add_numbers_failure():
+    assert add_numbers(2, 2) != 5
